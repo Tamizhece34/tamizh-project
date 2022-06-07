93eed52c6bfc282d67bef808be94148236f62069
Tamizhece34 <tamizhsgenba1@gmail.com>
2022-06-08 01:35:16 +0530

    hi

--- Tamizh_project/src/test/java/ta1.java
+++ Tamizh_project/src/test/java/ta1.java
@@ -3,4 +3,7 @@ public class ta1 {
 private void csk() {
 System.out.println("Chennai Super Kings");
 }
+private void mi() {
+	// TODO Auto-generated method stub
+System.out.println("mumbai indians");}
 }
