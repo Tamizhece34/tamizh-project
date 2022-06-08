a6ab27d9e053e023be314e8d5097f396fc5e1646
Tamizhece34 <tamizhsgenba1@gmail.com>
2022-06-09 00:59:57 +0530

    new

---
+++ Tamizh_project/src/test/java/demo1.java
@@ -0,0 +1,16 @@
+import java.awt.AWTException;
+import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo1 {
+
+	public static void main(String[] args) throws AWTException, IOException, InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sltam\\eclipse-workspace\\Sele\\Driver\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();
+	}
+		
+}
--- Tamizh_project/src/test/java/ta1.java
+++ Tamizh_project/src/test/java/ta1.java
@@ -4,6 +4,5 @@ private void csk() {
 System.out.println("Chennai Super Kings");
 }
 private void mi() {
-	// TODO Auto-generated method stub
 System.out.println("mumbai indians");}
 }
